c2a894cbab4a45719f411f4f9a63cd8f6444c6b2
Rafaely Zanão <rafaelyobzutzanao@gmail.com>
2024-08-05 14:33:02 -0300

    api filmes

--- src/app/components/MovieList/index.tsx
+++ src/app/components/MovieList/index.tsx
@@ -1,11 +1,51 @@
+'use client';
+
+import { APP_DIR_ALIAS } from 'next/dist/lib/constants';
 import './index.scss';
+import { use, useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 
 
 export default function MovieList(){
-   
+   const[movies, setMovies] = useState([]);
+   useEffect(()=>{
+    getMovies();
+   }, []);
+
+   function getMovies() {
+        axios({
+            method: 'get',
+            url: 'https://api.themoviedb.org/3/discover/movie',
+            params: {
+                api_key: 'eb648ec63f0f770b46471e7eb830f098',
+                language: 'pt-BR'
+            }
+        }).then(response => {
+            setMovies(response.data.results);
+            
+        });
+    }
+
     return(
         <ul className="MovieList">
-            <li></li>
+            { movies.map((movie)=>
+             <li className='movie-card'>
+                <p>
+                {movie.title}
+                </p>
+                <p className='description'>
+                {movie.overview}
+                </p>
+                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" />
+                <p>
+                    {movie.vote_average}
+                </p>
+             </li>
+
+        ) }
+           
         </ul>
     );
 }
