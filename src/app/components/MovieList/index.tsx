c873faa89188c9d6b470495fb27aeefa8d812fce
Rafaely Zanão <rafaelyobzutzanao@gmail.com>
2024-08-06 20:34:29 -0300

    avaliacoes

--- src/app/components/MovieList/index.tsx
+++ src/app/components/MovieList/index.tsx
@@ -4,12 +4,14 @@ import { APP_DIR_ALIAS } from 'next/dist/lib/constants';
 import './index.scss';
 import { use, useEffect, useState } from 'react';
 import axios from 'axios';
+import MovieCard from '../MovieCard';
+import { Movie } from '@/types';
 
 
 
 
 export default function MovieList(){
-   const[movies, setMovies] = useState([]);
+   const[movies, setMovies] = useState<Movie[]>([]);
    useEffect(()=>{
     getMovies();
    }, []);
@@ -31,19 +33,11 @@ export default function MovieList(){
     return(
         <ul className="MovieList">
             { movies.map((movie)=>
-             <li className='movie-card'>
-                <p>
-                {movie.title}
-                </p>
-                <p className='description'>
-                {movie.overview}
-                </p>
-                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" />
-                <p>
-                    {movie.vote_average}
-                </p>
-             </li>
-
+          
+                <MovieCard
+                 key={movie.id}
+                    movie={movie}               
+                />
         ) }
            
         </ul>
--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import './navbar/index.scss';
+import "./index.scss";
 
 export default function Navbar(){
     return(
